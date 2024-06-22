5b9a0ca7152d1406385fc344b7f7f009cb08bdae
iamanggi <anggipuspita452@gmail.com>
2024-06-22 12:59:35 +0700

    pp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gambar from "./assets/Foto-anggi.jpg";
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Selamat Hari Raya</h1>
+      <img src={gambar} alt="gambar" />
+
+    </>
+  )
+}
+
+export default App
