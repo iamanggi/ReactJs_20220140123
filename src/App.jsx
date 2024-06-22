29bd7d81c4fcb1b94432242e2583b522b6f6ebeb
iamanggi <anggipuspita452@gmail.com>
2024-06-22 17:59:13 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import gambar from "./assets/Foto-anggi.jpg";
+
+import gambar from './assets/Foto-anggi.jpg';
+import './Card.css';
 
 function App() {
- 
-
   return (
-    <>
-      <h1>Selamat Hari Raya</h1>
-      <img src={gambar} alt="gambar" />
-
-    </>
-  )
+    <div className="greeting-container">
+      <div className="greeting-card">
+        <h1>HAPPY EID AL-ADHA!!</h1>
+        <img src={gambar} alt="gambar" className="greeting-image" />
+        <p className="description">
+        Di hari yang penuh berkah ini, mari kita renungkan makna pengorbanan dan ketulusan. Selamat Idul Adha, mohon maaf lahir dan batin!!
+      </p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
